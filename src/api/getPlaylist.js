d0abee5c20a1b95ccc2c9b412d8ddaf29283d55d
nazim-uddin-limon <developernazim.dev@gmail.com>
2024-05-06 22:00:51 +0600

    resove small issue

--- src/api/getPlaylist.js
+++ src/api/getPlaylist.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-const API_KEY = "AIzaSyDTnvBxKOUiNyuR7dDYIrErawGZF_q9Vws";
 
 const getPlaylistItems = async (playlistId, pageToken = "", result = []) => {
-  const URL = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails,snippet&pageToken=${pageToken}&maxResults=100&playlistId=${playlistId}&key=${API_KEY}`;
+  const URL = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails,snippet&pageToken=${pageToken}&maxResults=100&playlistId=${playlistId}&key=${
+    import.meta.env.VITE_YOUTUBE
+  }`;
 
   const { data } = await axios.get(URL);
   result = [...result, ...data.items];
@@ -12,11 +13,13 @@ const getPlaylistItems = async (playlistId, pageToken = "", result = []) => {
   return result;
 };
 const getPlaylist = async (playlistId) => {
-  const URL = `https://youtube.googleapis.com/youtube/v3/playlists?part=contentDetails,snippet&maxResults=100&id=${playlistId}&key=${API_KEY}`;
+  const URL = `https://youtube.googleapis.com/youtube/v3/playlists?part=contentDetails,snippet&maxResults=100&id=${playlistId}&key=${
+    import.meta.env.VITE_YOUTUBE
+  }`;
 
   const { data } = await axios.get(URL);
   const playlistItems = await getPlaylistItems(playlistId);
-  console.log(playlistItems)
+  console.log(playlistItems);
   return {
     playlistDetails: data,
     playlistItems,
